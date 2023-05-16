971025cac205e05da8c51eae34029c5c62f3c5a1
Mark Mueller <92736704+MuellMark@users.noreply.github.com>
2023-05-16 11:36:25 -0500

    #2 Made into a function
    Made it into a function to call and added comments

--- fiveVectorTest.py
+++ fiveVectorTest.py
@@ -1,22 +1,23 @@
 
-# Will need to change for machine
-file = open('/Users/markymarkscomputer/Desktop/Untitled/fiveVec1.txt', 'r')
-content = file.readlines()
+def readFromFile(edges):
+    # Will need to change for machine
+    file = open('/Users/markymarkscomputer/Desktop/Untitled/fiveVec1.txt', 'r')
+    content = file.readlines()
 
-edges=[]
+    for line in content:
+        tempNode = [0,0]
+        index =0
+        for i in line:
+            if(i.isdigit()):
+                tempNode[index]=int(i)
+                index+=1
+        #print(tempNode)
+        edges.append(tempNode)
+    #print(edges)   
 
-for line in content:
-    tempNode = [0,0]
-    index =0
-    for i in line:
-        if(i.isdigit()):
-            tempNode[index]=int(i)
-            index+=1
-    #print(tempNode)
-    edges.append(tempNode)
-print(edges)   
+    file.close()
 
-         
-    
- 
-file.close()
+
+edges=[] #Stores all the edges
+readFromFile(edges)
+print(edges)  
