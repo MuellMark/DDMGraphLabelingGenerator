eb082b1b8080f7fb84fed4c053b41eefd05eea56
Mark Mueller <92736704+MuellMark@users.noreply.github.com>
2023-05-16 12:03:15 -0500

    #2 getSums working
    needs comments, need to make verifier that checks that all the ins and outs are the same

--- fiveVectorTest.py
+++ fiveVectorTest.py
@@ -5,19 +5,43 @@ def readFromFile(edges):
     content = file.readlines()
 
     for line in content:
-        tempNode = [0,0]
+        tempNode = [0,0]  #list that takes in a temp pairing
         index =0
         for i in line:
             if(i.isdigit()):
                 tempNode[index]=int(i)
                 index+=1
-        #print(tempNode)
-        edges.append(tempNode)
-    #print(edges)   
+        edges.append(tempNode) #Appends to the list of edges
 
     file.close()
+
+def getSums(edges,sumsPerEdge):
+    #Init the sums list to have 5 empty "tuples"
+    i=0
+    while(i<5):
+        tempPair =[0,0]
+        sumsPerEdge.append(tempPair)
+        i+=1
+
+    for i in edges:
+        indexOut = i[0]-1
+        indexIn = i[1]-1
+        sumsPerEdge[indexOut][1]+=i[1]
+        sumsPerEdge[indexIn][0]+=i[0]
+        # Debugging Statements
+        # print(i)
+        # print(sumsPerEdge)
+    
+
+#Will verify once the other list is filled in
+def verifier():
+    print("TODO")
 
 
 edges=[] #Stores all the edges
 readFromFile(edges)
-print(edges)  
+#print(edges) 
+
+sumsPerEdge=[]
+getSums(edges,sumsPerEdge)
+print(sumsPerEdge) 
