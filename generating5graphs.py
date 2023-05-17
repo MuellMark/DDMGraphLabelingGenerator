9fa467945d44edeb3e101ccc446840fb6c615066
Mark Mueller <92736704+MuellMark@users.noreply.github.com>
2023-05-17 16:38:34 -0500

    #4 added comments

--- generating5graphs.py
+++ generating5graphs.py
@@ -7,6 +7,7 @@ def initListOfLists(list,size):
         list.append(tempPair)
         i+=1
 
+#Computes sums of vertices going in and out, same as in other file
 def computeSums(edges,sumofedges):
     for i in edges:
         indexOut = i[0]-1
@@ -14,11 +15,14 @@ def computeSums(edges,sumofedges):
         sumofedges[indexOut][1]+=i[1]  #Going out
         sumofedges[indexIn][0]+=i[0]   #Coming in
 
+#computes the number of edges going in and out, same as sums, but without the edge weights
 def computeInvsOut(edges,invsout):
     for i in edges:
         invsout[i[1]-1][0]+=1
         invsout[i[0]-1][1]+=1
 
+#Checks if any edges have less than 3 coming in+going out, returns first one that is like that
+#May need to change or add another method that makes sure 1 in each in and out
 def checkInvsOut(invsout):
     valNeedsMore =0
     i=0
@@ -29,21 +33,26 @@ def checkInvsOut(invsout):
         i+=1
     return valNeedsMore
 
-
+#Focus on generating one graph first, then see how to generate more before stopping again
+#After checking, check which one has too many, take one from there, replace the with the new
+#one in the direction that has too many (so first, replace ones that start from 1, will need to
+# be new method)
 def generatePossibleGraph(edges,sumofedges,possibleGraphs, invsout):
     computeInvsOut(edges,invsout)
     print(checkInvsOut(invsout))
 
 edges=[[1,2],[1,3],[1,4],[1,5],[2,1],[2,3],[2,4],[2,5]] #Stores all the edges
-sumofedges=[]
+sumofedges=[] #stores sum of edges
 initListOfLists(sumofedges,5)
 computeSums(edges,sumofedges)
 
-invsout=[]
+invsout=[] #stores edges coming in vs out
 initListOfLists(invsout,5)
 
 possibleGraphs=[] #stores all possible graphs that were made
 generatePossibleGraph(edges,sumofedges,possibleGraphs,invsout)
+
+#debugging
 print(edges)
 print(sumofedges)
 print(invsout)
