9cf15ab27c31c75bdeb144fd2536d89922f6a846
Mark Mueller <92736704+MuellMark@users.noreply.github.com>
2023-05-17 16:15:11 -0500

    #4 added checkinvsout
    by keeping track of how many ins and outs there are, we should be able to generate at least one possible graphs

--- generating5graphs.py
+++ generating5graphs.py
@@ -19,10 +19,20 @@ def computeInvsOut(edges,invsout):
         invsout[i[1]-1][0]+=1
         invsout[i[0]-1][1]+=1
 
+def checkInvsOut(invsout):
+    valNeedsMore =0
+    i=0
+    while(i<5):
+        if(invsout[i][0]+invsout[i][1]<3):
+            valNeedsMore = i+1
+            i+=5
+        i+=1
+    return valNeedsMore
+
+
 def generatePossibleGraph(edges,sumofedges,possibleGraphs, invsout):
     computeInvsOut(edges,invsout)
-    for i in edges:
-        print(i)
+    print(checkInvsOut(invsout))
 
 edges=[[1,2],[1,3],[1,4],[1,5],[2,1],[2,3],[2,4],[2,5]] #Stores all the edges
 sumofedges=[]
