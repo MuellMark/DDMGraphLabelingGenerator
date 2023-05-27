50eb942b7fbf025caf2ecaaccb7150406819dfe2
Mark Mueller <92736704+MuellMark@users.noreply.github.com>
2023-05-27 14:34:22 -0500

    #7 continuing java

--- javaGen5Graphs.java
+++ javaGen5Graphs.java
@@ -1,5 +1,8 @@
 class javaGen5Graphs{
     public static void main (String []args){
-        System.out.println("AAAA");
+        // first have an array of size 2 that stores an edge 
+        // Them have an array of size 8 storing the edges, 8 needs to be a var
+        // Then have an array list to hold all graphs, then go from there
+        System.out.println("ADfg");
     }
 }
