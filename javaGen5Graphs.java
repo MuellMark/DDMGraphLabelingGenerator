3b3def5d1fd8ed94217fbebd6d9094e227ffb036
Mark Mueller <92736704+MuellMark@users.noreply.github.com>
2023-05-29 12:24:58 -0500

    #4 starting point for iterative code

--- javaGen5Graphs.java
+++ javaGen5Graphs.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 
 class javaGen5Graphs{
     public static void main (String []args){
+        //Initializes an array of edges at the start
         int[][] edges =new int[8][2];
         for(int i=0;i<8;i++){
             for(int j=0;j<2;j++){
@@ -9,11 +10,23 @@ class javaGen5Graphs{
             }
         }
 
+        // Test for generate, will have 6 total edges to add instead of 8
+        int[][] test =new int[3][2];
+        for(int i=0;i<3;i++){
+            for(int j=0;j<2;j++){
+                test[i][j]=1;
+            }
+        }
+
+        //Stores all possible graphs
         ArrayList <int[][]> possibleGraphs= new ArrayList<int[][]>();
         possibleGraphs.add(edges);
-        // Then have an array list to hold all graphs, then go from there
-        //System.out.print("ASDFGSDA\n");
-        printEdges(possibleGraphs.get(0));
+
+        //printEdges(possibleGraphs.get(0));
+
+        possibleGraphs=generate(possibleGraphs, test);
+        printPossibleGraphs( possibleGraphs);
+
     }
 
     // Prints the edges in a readable format
@@ -26,5 +39,23 @@ class javaGen5Graphs{
             }
             System.out.print("] ");
         }
+    }
+
+    public static void printPossibleGraphs(ArrayList <int[][]> possibleGraphs){
+       for(int i=0;i<possibleGraphs.size();i++){
+        printEdges(possibleGraphs.get(i));
+       } 
+    }
+
+    //TODO: This method will be for generating the graphs
+    // inputs: a start arr of edges, and the arraylist
+    // output: arraylist, to be used to test
+    public static ArrayList <int[][]> generate(ArrayList <int[][]> possibleGraphs,int[][] startEdges){
+        //First, I need a starting point for the array of edges
+        // I'm going to try an iterative approach that first goes through all possible permutations
+        // Checking each one, then seeing if I can scale it down
+
+        System.out.print("TODO");
+        return possibleGraphs;
     }
 }
