e7a3ca3c7a97adf26f902ea223f98dc9805d2678
Mark Mueller <92736704+MuellMark@users.noreply.github.com>
2023-05-29 11:23:05 -0500

    #4 Created arraylist of 2d arrays

--- javaGen5Graphs.java
+++ javaGen5Graphs.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 class javaGen5Graphs{
     public static void main (String []args){
         int[][] edges =new int[8][2];
@@ -6,13 +8,15 @@ class javaGen5Graphs{
                 edges[i][j]=1;
             }
         }
-        // first have an array of size 2 that stores an edge 
-        // Them have an array of size 8 storing the edges, 8 needs to be a var
+
+        ArrayList <int[][]> possibleGraphs= new ArrayList<int[][]>();
+        possibleGraphs.add(edges);
         // Then have an array list to hold all graphs, then go from there
         //System.out.print("ASDFGSDA\n");
-        printEdges(edges);
+        printEdges(possibleGraphs.get(0));
     }
 
+    // Prints the edges in a readable format
     public static void printEdges(int[][] edges){
         for(int i=0;i<8;i++){
             System.out.print("[");
