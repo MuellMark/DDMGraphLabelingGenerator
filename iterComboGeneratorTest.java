a9c205205f1e2a1e35080f42e200c6f2337850df
Mark Mueller <92736704+MuellMark@users.noreply.github.com>
2023-05-30 09:58:56 -0500

    #4 getting stuck at index 2
    currently infinite loop

--- iterComboGeneratorTest.java
+++ iterComboGeneratorTest.java
@@ -5,17 +5,22 @@ class iterComboGeneratorTest{
         int[] test = {1,1,1,1};
         ArrayList <int[]> combos= new ArrayList<int[]>();
         generate(combos, test);
-        printCombos(combos);
+        //printCombos(combos);
     }
 
     public static void generate(ArrayList <int[]> combos,int[] test){
+        
         int index=0;
         int adding=0;
-        while(test[3]<5){ //Will need to change
+        while(combos.size()<625){ //Will need to change
+            for(int i=0;i<test.length;i++){
+                System.out.print(test[i]+",");
+            }
+            System.out.println();
             combos.add(copyArr(test));
-
-            if(adding==index &&test[adding]==5){
-                
+            //System.out.println(combos.size());
+            if(checkAllFives(test, index)){
+                System.out.print("happens");
                 index++;
                 test[index]++;
                 adding=0;
@@ -23,15 +28,29 @@ class iterComboGeneratorTest{
                     test[i]=1;
                 }
 
-            }
-
-            if(test[adding]<5){
+            }else if(test[adding]<5){
+                
                 test[adding]++;
-            }else{ adding++;}
+            }else{
+                // need a for loop
+                 test[adding+1]++;
+                 for(int i=0;i<adding+1;i++){
+                    test[i]=1;
+                }
+                adding=0;
 
+            }
+            
             //System.out.println(test[0]);
             
         }
+    }
+
+    public static boolean checkAllFives(int[]arr, int end){
+        for(int i=0;i<=end;i++){
+            if(!(arr[i]==5)) return false;
+        }
+        return true;
     }
 
     public static int[] copyArr(int[] srcArr){
