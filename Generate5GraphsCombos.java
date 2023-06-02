6299808631b7d17bc7606b5697ddac9f40c51bb7
Mark Mueller <92736704+MuellMark@users.noreply.github.com>
2023-06-02 16:05:00 -0500

    #10 reworked the structure to hold edges

--- Generate5GraphsCombos.java
+++ Generate5GraphsCombos.java
@@ -16,7 +16,28 @@ class generate5GraphsCombos{
         error[0][0]=-1;
         edges.add(error);
         for(int i=1;i<6;i++){
+            edges.add(new int[2][4]);
             // Need input and output for each possible vertex
         }
+
+        // ArrayList<Integer>[] test = new ArrayList[2];
+        // for (int i = 0; i < 2; i++) {
+        //     test[i] = new ArrayList<Integer>();
+        // }
+        // System.out.println(test[0].size());
+        // NEW DECLARATION OF EDGES:
+        ArrayList<Integer>[][] edges2 = new ArrayList[6][2];
+        for(int i=1;i<6;i++){
+            ArrayList<Integer>[] test = new ArrayList[2];
+            for (int j = 0; j < 2; j++) {
+                test[j] = new ArrayList<Integer>();
+            }
+            edges2[i]=test;
+        }
+        edges2[2][1].add(53);
+        System.out.println(edges2[2][1].get(0));
+        System.out.println(edges2[3][1].get(0));
+
+
     }
 }
