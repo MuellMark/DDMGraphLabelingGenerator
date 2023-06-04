fb028b36aa9e2a1d9a3a592929cbd31d00cbcbf4
Mark Mueller <92736704+MuellMark@users.noreply.github.com>
2023-06-03 22:10:30 -0500

    #11 added print and add methods

--- testEdges.java
+++ testEdges.java
@@ -4,6 +4,7 @@ import java.util.*;
 class edgeStorage{
     ArrayList<Integer>[][] edges;
 
+    // Constructor to specify size
     public edgeStorage(int size){
         size++;
         edges = new ArrayList[size][2];
@@ -15,6 +16,7 @@ class edgeStorage{
             edges[i]=test;
         }
     }
+    //Default Constructor
     public edgeStorage(){
         edges = new ArrayList[6][2];
         for(int i=1;i<6;i++){
@@ -25,11 +27,46 @@ class edgeStorage{
             edges[i]=test;
         }
     }
+    //TODO: getter methods, probably a copy method
+    //prints edges in a easily readable way
+    public void print(){
+        for(int i=1;i<edges.length;i++){
+            System.out.print("Vertex "+i+":");
+            System.out.print(" [i:");
+            for(int j=0;j<edges[i][1].size();j++){
+                System.out.print(" "+edges[i][1].get(j));
+            }
+            System.out.print("]\n          [o:");
+            for(int j=0;j<edges[i][0].size();j++){
+                System.out.print(" "+edges[i][0].get(j));
+            }
+            System.out.println("]");
+        }
+    }
+
+    // Adds element to inputs at vertex
+    public void addIn(int vertex, int element){
+        edges[vertex][1].add(element);
+    }
+    // Adds element to outputs at vertex
+    public void addOut(int vertex, int element){
+        edges[vertex][0].add(element);
+    }
+
+    // Adds a pair of elements from -> to
+    public void addPair(int from,int to){
+        addOut(from,to);
+        addIn(to,from);
+    }
+
 }
+// Want to move all the code over here for testing
 class testEdges{
     public static void main(String[]args){
+        ArrayList<edgeStorage> AllCombos = new ArrayList<>(); //Will store all possible combos I'm working with
         edgeStorage test = new edgeStorage(5);
-        System.out.println(test.edges[0][0]);
+        test.addPair(5,2);
+        test.print();
     }
 
 }
