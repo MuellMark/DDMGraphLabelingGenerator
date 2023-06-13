930b5cdd56b6edf325067cea7380a4c7e40f94d2
Mark Mueller <92736704+MuellMark@users.noreply.github.com>
2023-06-12 20:18:26 -0500

    #19 added title

--- graphVisTest.py
+++ graphVisTest.py
@@ -43,6 +43,7 @@ G.add_edges_from([(1, 2), (1, 3),(2,5),(3,5),(4,2),(4,3),(5,1),(5,4)])
 #G.visualize()
 
 #G = nx.complete_graph(5)
+plt.title("5 Vertex Graph #[number]")
 nx.draw(G,node_color='white',with_labels=True)
 plt.savefig("/Users/markymarkscomputer/Desktop/Untitled/graphVisTest.png")
 plt.clf()
