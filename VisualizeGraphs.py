23014e11fc6ac2fee688511327ef2f7896e3a68d
Mark Mueller <92736704+MuellMark@users.noreply.github.com>
2023-06-13 15:29:47 -0500

    #19 5 graphs no inverses generated

--- VisualizeGraphs.py
+++ VisualizeGraphs.py
@@ -7,8 +7,29 @@ inverses = False
 
 graphnum=1 #Tracks graph number for file
 
-graphsToGenerate= open("/Users/markymarkscomputer/Desktop/Untitled/graphVisTestGraph.txt",'r')
+graphsToGenerate= open("/Users/markymarkscomputer/Desktop/Untitled/outputForVis.txt",'r')
 contents = graphsToGenerate.readlines()
+for line in contents:
+    index =0
+    tempList=[]
+    G = nx.DiGraph()
+    while index<len(line):
+        #Need functionality for naming maybe
+        if line[index].isdigit():
+            ifrom=int(line[index])
+            index+=2
+            ito=int(line[index])
+            tempTuple = (ifrom,ito)
+            tempList.append(tempTuple)
+        index+=1
 
+    print(tempList)
+    G.add_edges_from(tempList)
+    plt.title("5 Vertex Graph #[number]")
+    nx.draw(G,node_color='white',with_labels=True)
+    plt.savefig("/Users/markymarkscomputer/Desktop/Untitled/GraphVisualizations/5VertexNoInverse/graph"+str(graphnum)+".jpeg")
+    plt.clf()
+    G.clear()
+    graphnum+=1
 
 #plt.savefig("/Users/markymarkscomputer/Desktop/Untitled/GraphVisualizations/5VertexNoInverse/graph"+str(graphnum)+".jpeg")
