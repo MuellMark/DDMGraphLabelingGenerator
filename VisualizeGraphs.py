6cefd832970eceaf259de7ed29ae25bb270d6152
Mark Mueller <92736704+MuellMark@users.noreply.github.com>
2023-06-13 21:31:20 -0500

    #19 added 6 vertex graphs

--- VisualizeGraphs.py
+++ VisualizeGraphs.py
@@ -23,17 +23,27 @@ for line in contents:
             tempList.append(tempTuple)
         index+=1
 
-    G.add_node(1,pos=(2,1))
-    G.add_node(2,pos=(4,1))
-    G.add_node(3,pos=(1,2))
-    G.add_node(4,pos=(5,2))
-    G.add_node(5,pos=(3,3))
+    if(numVertices==5):
+        G.add_node(1,pos=(2,1))
+        G.add_node(2,pos=(4,1))
+        G.add_node(3,pos=(1,2))
+        G.add_node(4,pos=(5,2))
+        G.add_node(5,pos=(3,3))
+    elif(numVertices==6):
+        G.add_node(1,pos=(1,2))
+        G.add_node(2,pos=(2,1))
+        G.add_node(3,pos=(3,2))
+        G.add_node(4,pos=(3,3))
+        G.add_node(5,pos=(2,4))
+        G.add_node(6,pos=(1,3))
     pos=nx.get_node_attributes(G,'pos')
 
     print(tempList)
     G.add_edges_from(tempList)
     plt.title(str(numVertices)+" Vertex Graph #"+str(graphnum))
     nx.draw(G,pos,node_color='white',with_labels=True)
+
+    # Need to change save location based on what is generated
     plt.savefig("/Users/markymarkscomputer/Desktop/Untitled/GraphVisualizations/5VertexNoInverse/graph"+str(graphnum)+".jpeg")
     plt.clf()
     G.clear()
