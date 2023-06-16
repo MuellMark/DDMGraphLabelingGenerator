1c43a19e8c17fbf64f7e82082145bfd05163de1d
Mark Mueller <92736704+MuellMark@users.noreply.github.com>
2023-06-16 05:45:26 -0500

    #24 Cleaned up OptimizationTest

--- generateGraphsOptimizationTest.java
+++ generateGraphsOptimizationTest.java
@@ -6,54 +6,50 @@ import java.io.FileWriter;
 
 class generateGraphsOptimizationTest{
     public static void main(String[]args){
-        int numVertices =9; // Change for # of Vertices
+        int numVertices =7; // Change for # of Vertices
 
         // Stores all possible combinations for all recurssive calls
         ArrayList<edgeStorage> allCombos = new ArrayList<>();
+        // Stores all ddmLabelings as the recurssive method runs through
         ArrayList<edgeStorage> ddmLabelings = new ArrayList<>();
 
-        //Starting vertex is all 0's
+        // StartGraph is all zeroes
         edgeStorage startGraph = new edgeStorage(numVertices);
         allCombos.add(startGraph);
 
         generate(allCombos, numVertices,ddmLabelings); // Starts generating graphs
-        // System.out.println("Finished! "+allCombos.size()+" Graphs generated, need to sort throught them...");
-        // sortAllCombos(allCombos); // Sort values for easier comparing
-        // ArrayList<edgeStorage> filtered = filterResults(allCombos); //Removes repeats & invalid graphs
-        // System.out.println("Finished first filter!! "+filtered.size()+" Graphs are in allCombos");
-        // // Removes inverses, comment out if inverses wanted
-        // ArrayList<edgeStorage> inverseFiltered = filterInverseResults(filtered);
 
-        // Prints all graphs, need to change if inverses wanted
+        // Prints all graphs found, then writes to files
+        // Several different version, change depending on desired output
         printAllAdjMatrix(ddmLabelings);
         writeAllCombosToFileVisualization(ddmLabelings);
     }
 
     // Loops through all recursive calls from checkforSums
     public static void generate(ArrayList<edgeStorage> allCombos, int vertex,ArrayList<edgeStorage> ddmLabelings){
-        // Loops through all vertices
+        // Loops through all vertices in reverse order
         while(vertex>0){
-            
-            ArrayList<Integer> set = getUsuableSet(vertex);
+            ArrayList<Integer> set = getUsuableSet(vertex); // Gets usable edge set
             int currentSize=allCombos.size(); // Makes sure not an infinite loop, only checks current graphs
-            System.out.println(currentSize);
+            System.out.println("Graphs generated so far: "+currentSize);
+            // Does a recurrsive call to all graphs in allCombos
             for(int i=0;i<currentSize;i++){
                 edgeStorage temp = allCombos.get(i).copy(); // Creates copy to avoid pointer issues
-                checkForSums(allCombos, temp, set, vertex, 0,ddmLabelings);
+                checkForSums(allCombos, temp, set, vertex, 0,ddmLabelings); // Recursive call
             }
             vertex--;
         }
     }
 
-    // Recursive method, finds all possible combinations of edges
+    // Recursive method, finds all possible combinations of edges TODO: potentailly add a max value to check
     public static void checkForSums(ArrayList<edgeStorage> allCombos, edgeStorage current,
     ArrayList<Integer> set, int vertex, int index, ArrayList<edgeStorage> ddmLabelings){
-        //int maxVal = 10; // Place holder, will need a way to calculate it, may be able to remove it
         int sumIns = current.getSumIns(vertex);
         int sumOuts = current.getSumOuts(vertex);
 
+        // base case, if the two are equal then a potential labeling could be found
         if(sumIns==sumOuts && sumIns>0 ){
-            allCombos.add(current); 
+            allCombos.add(current); // Adds to allCombos for future graphs to check from
             if(current.isDDMLabeling()){
                 boolean isInvOrEq = false;
                 for(int i=0;i<ddmLabelings.size();i++){
@@ -62,12 +58,13 @@ class generateGraphsOptimizationTest{
                         i+=allCombos.size();
                     }
                 }
+                // If the graph is a valid labeling, is not a repear or inverse, then it's added to ddmLabelings
                 if(!isInvOrEq){
-                    //System.out.println("Graph added, "+ddmLabelings.size()+"graphs found");
                     ddmLabelings.add(current);
                 }
             }
-        }else if(index<set.size()){// Recurssive, may need something with the max
+        // Otherwise, does a recurrsive case
+        }else if(index<set.size()){
             // Creates copy and adds index to ins
             edgeStorage addToIns = current.copy();
             addToIns.addPair(set.get(index),vertex);
@@ -76,13 +73,14 @@ class generateGraphsOptimizationTest{
             edgeStorage addToOuts = current.copy();
             addToOuts.addPair(vertex,set.get(index));
             
-            // Creates copy, does not add index
+            // Creates copy, does not add index to either ins or outs
             edgeStorage copyCurrent = current.copy(); 
-            index++;
-            if(allCombos.size()%10000==0)
-            System.out.println("Graph added, "+allCombos.size()+" graphs found");
+            index++; // Incremenets index
 
-            //Recursive calls
+            // Debugging statement, to track number of graphs generated when dealing with large cases
+            if(allCombos.size()%10000==0) System.out.println("Graph added, "+allCombos.size()+" graphs found");
+
+            //Recursive calls, one for each new graph created
             checkForSums(allCombos,copyCurrent,set,vertex,index,ddmLabelings);
 
             checkForSums(allCombos,addToIns,set,vertex,index,ddmLabelings);
@@ -91,7 +89,7 @@ class generateGraphsOptimizationTest{
 
         }
     }
-    //Gets usable set for a vertex
+    //Gets usable set for a vertex, basically all vertices less than the input
     public static ArrayList<Integer> getUsuableSet(int vertex){
         ArrayList<Integer> set = new ArrayList<>();
         for(int i=1;i<vertex;i++){
@@ -100,7 +98,7 @@ class generateGraphsOptimizationTest{
         return set;
     }
 
-    // Prints arraylist of edgeStorage
+    // Prints arraylist of edgeStorage in a readable fassion
     public static void printAllCombos(ArrayList<edgeStorage> AllCombos){
         for(int i=0;i<AllCombos.size();i++){
             System.out.println("Graph #"+(i+1)+":");
@@ -108,7 +106,7 @@ class generateGraphsOptimizationTest{
         }
     }
 
-        // Prints arraylist of edgeStorage
+        // Prints arraylist of edgeStorage in an adjacency matrix for easy checking
         public static void printAllAdjMatrix(ArrayList<edgeStorage> AllCombos){
             for(int i=0;i<AllCombos.size();i++){
                 System.out.println("Graph #"+(i+1)+":");
@@ -116,6 +114,7 @@ class generateGraphsOptimizationTest{
             }
         }
 
+    // LEGACY, keeping in case needed in future 
     // Filters results, stores all non-repeated DDM labelings in a new Arraylist, returns that
     public static ArrayList<edgeStorage> filterResults(ArrayList<edgeStorage> allCombos){
         ArrayList<edgeStorage> filtered = new ArrayList<>(); // Stores the filtered results
@@ -131,7 +130,9 @@ class generateGraphsOptimizationTest{
         return filtered;
     }
 
-    // If a graph is just the inverse of another graph, not kept
+    // LEGACY, keeping in case needed in future 
+    // If a graph is just the inverse of another graph, not kept, to be used in addition to 
+    // filterResults, not instead of it
     public static ArrayList<edgeStorage> filterInverseResults(ArrayList<edgeStorage> allCombos){
         ArrayList<edgeStorage> filtered = new ArrayList<>(); // Stores the filtered results
         for(int i=0;i<allCombos.size();i++){
@@ -144,67 +145,59 @@ class generateGraphsOptimizationTest{
         return filtered;
     }
 
-    // calls sort on every graph in allCombos
+    // calls sort on every graph in allCombos, sorting the ins and outs for easier checking
     public static void sortAllCombos(ArrayList<edgeStorage> allCombos){
         for(int i=0;i<allCombos.size();i++){
             allCombos.get(i).sort();
         }
     }
 
-    // Prints arraylist of edgeStorage
+    // Writes the arraylist of combinations to a file in a readbale way
     public static void writeAllCombosToFile(ArrayList<edgeStorage> AllCombos){
         try {
+            // Opens files
             FileWriter myWriter = new FileWriter("/Users/markymarkscomputer/Desktop/Untitled/output.txt");
             for(int i=0;i<AllCombos.size();i++){
-            
                 myWriter.write("\nGraph #"+(i+1)+":\n");
-                
                 AllCombos.get(i).writeToFile(myWriter);
             }
             myWriter.close();
-              } catch (IOException e) {
-                System.out.println("An error occurred.");
-                e.printStackTrace();
-            }
-            
-        
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+ 
     }
 
-    // Prints arraylist of edgeStorage
+    // Writes to a file in an adjacency matrix for manual checking
     public static void writeAllCombosToFileAdjMatrix(ArrayList<edgeStorage> AllCombos){
         try {
+            // Opens file
             FileWriter myWriter = new FileWriter("/Users/markymarkscomputer/Desktop/Untitled/output.txt");
             for(int i=0;i<AllCombos.size();i++){
-            
                 myWriter.write("\nGraph #"+(i+1)+":\n");
-                
                 AllCombos.get(i).writeToFileAdjMat(myWriter);
             }
             myWriter.close();
-              } catch (IOException e) {
-                System.out.println("An error occurred.");
-                e.printStackTrace();
-            }
-            
-        
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
     }
 
+    // Writes to file to be read in by visualizeGraphs.py
     public static void writeAllCombosToFileVisualization(ArrayList<edgeStorage> AllCombos){
         try {
+            // Opens File
             FileWriter myWriter = new FileWriter("/Users/markymarkscomputer/Desktop/Untitled/outputForVis.txt");
             for(int i=0;i<AllCombos.size();i++){
-            
-                
-                
                 AllCombos.get(i).writeToFileForVisualization(myWriter);
                 myWriter.write("\n");
             }
             myWriter.close();
-              } catch (IOException e) {
-                System.out.println("An error occurred.");
-                e.printStackTrace();
-            }
-            
-        
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
     }
 }
