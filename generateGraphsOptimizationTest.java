87f3227d808cbf0987437a6331d175314ade839f
Mark Mueller <92736704+MuellMark@users.noreply.github.com>
2023-06-18 19:51:09 -0500

    Moving many files around

--- generateGraphsOptimizationTest.java
+++ generateGraphsOptimizationTest.java
@@ -6,7 +6,7 @@ import java.io.FileWriter;
 
 class generateGraphsOptimizationTest{
     public static void main(String[]args){
-        int numVertices =9; // Change for # of Vertices
+        int numVertices =5; // Change for # of Vertices
 
         // Stores all possible combinations for all recurssive calls
         ArrayList<edgeStorage> allCombos = new ArrayList<>();
